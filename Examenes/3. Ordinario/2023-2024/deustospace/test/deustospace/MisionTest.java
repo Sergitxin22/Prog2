a628d54eec46eea99ab656e0a288e8faa79380f5
Sergitxin <65020729+Sergitxin22@users.noreply.github.com>
2024-06-18 20:56:43 +0200

    Format MisionTest.java file

--- Examenes/3. Ordinario/2023-2024/deustospace/test/deustospace/MisionTest.java
+++ Examenes/3. Ordinario/2023-2024/deustospace/test/deustospace/MisionTest.java
@@ -33,45 +33,47 @@ class MisionTest {
 		// Primer caso
 		Mision m1 = new Mision("mision1", "madrid", "marte", 2024, 2, 20);
 		Nave n1 = new Nave("nave1", "proveedor1", 0, 5);
+		
 		m1.setNave(n1);
 		assertEquals(0, m1.getPersonal().size());
 		assertEquals(0, m1.getNave().getCoste());
 		
 		// costeInicial + costePersonal + costeNave
-		//     1.5		+       0		+     0		= 1.5
+		//     1.5	+       0	+     0	   = 1.5
 		assertEquals(1.5, m1.getCosteTotal());
 		
 		// Segundo caso
 		Mision m2 = new Mision("mision2", "madrid", "marte", 2024, 2, 20);
 		Nave n2 = new Nave("nave2", "proveedor1", 5, 5);
+		
 		m2.setNave(n2);
 		assertEquals(0, m2.getPersonal().size());
 		assertEquals(5, m2.getNave().getCoste());
 		
 		// costeInicial + costePersonal + costeNave
-		//     1.5		+       0		+     5		= 6.5
+		//     1.5	+       0	+     5	   = 6.5
 		assertEquals(6.5, m2.getCosteTotal());
 		
 		// Tercer caso
 		Mision m3 = new Mision("mision3", "madrid", "marte", 2024, 2, 20);
-
 		Astronauta a1 = new Astronauta("sergio", "spain");
+		
 		m3.setNave(n2);
 		ArrayList<Personal> p1 = new ArrayList<Personal>();
 		p1.add(a1);
 		m3.setPersonal(p1);
 		assertEquals(1, m3.getPersonal().size());
 		assertEquals(0, (((Astronauta) m3.getPersonal().get(0)).getHabilidades()).size());
-		
+
+		// costePersonal = 0.25*0
 		// costeInicial + costePersonal + costeNave
-		//     1.5		+   0.25*0=0	+     5		= 6.5
+		//     1.5	+       0 	+     5    = 6.5
 		assertEquals(6.5, m3.getCosteTotal());
-		//System.out.println(m3.getCosteTotal());
 		
 		// Cuarto caso
 		Mision m4 = new Mision("mision4", "madrid", "marte", 2024, 2, 20);
-
 		ArrayList<Habilidad> h1 = new ArrayList<>();
+		
 		h1.add(Habilidad.INGENIERIA);
 		h1.add(Habilidad.INVESTIGACION);
 		Astronauta a2 = new Astronauta("sergio", "spain");
@@ -82,9 +84,10 @@ class MisionTest {
 		m4.setPersonal(p2);
 		assertEquals(1, m4.getPersonal().size());
 		assertEquals(2, (((Astronauta) m4.getPersonal().get(0)).getHabilidades()).size());
+		
 		// costePersonal = 0.25*2*0.1
 		// costeInicial + costePersonal + costeNave
-		//     1.5		+     0.05		+     5		= 6.55
+		//     1.5	+     0.05	+     5	   = 6.55
 		assertEquals(6.55, m4.getCosteTotal());
 		
 		// Quinto caso
@@ -102,7 +105,7 @@ class MisionTest {
 		
 		// costePersonal = (0.25*2*0.1) + (0.15*1) + (0.15*2)
 		// costeInicial + costePersonal + costeNave
-		//     1.5		+     0.5		+     5		= 7
+		//     1.5	+     0.5	+     5	   = 7
 		assertEquals(7, m5.getCosteTotal());
 	}
 
